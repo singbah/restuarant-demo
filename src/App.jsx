5d79d1390472b55d134775b7a67e3a4521163f9c
John P. Singbah <singbahjohn@gmail.com>
2026-06-15 00:11:01 +0000

    edited some stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import HeroSection from "./components/hero"
 import { FanIcon } from "lucide-react"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useNavigate } from "react-router-dom"
 import MenuSection from "./components/menu"
 import AboutUs from "./components/about"
 import ContactSection from "./components/contact"
@@ -12,13 +12,16 @@ const iconStyle = "inline"
 
 
 export default function App() {
+  const navigate = useNavigate()
+
   return (<div className="relative">
     <header className="bg-black text-white sticky top-0 flex justify-center items-center py-3 gap-3 z-50">
       <nav className="flex justify-center items-center gap-2">
         <a href="/" className={navAStyle}>Home</a>
-        <a href="/menu" className={navAStyle}>Menu</a>
-        <a href="/contact" className={navAStyle}>Contact</a>
-        <a href="about-us" className={navAStyle}>About</a>
+        <a onClick={() => navigate("menu")} className={navAStyle}>Menu</a>
+        <a onClick={() => navigate("contact")} className={navAStyle}>Contact</a>
+        <a onClick={() => navigate("about-us")}
+          className={navAStyle}>About</a>
 
       </nav>
     </header>
