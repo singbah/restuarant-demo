12637adbd1e8acc6047f9fe528b155fb14fd273b
John P. Singbah <singbahjohn@gmail.com>
2026-06-15 10:36:57 +0000

    a

--- src/components/restaurants.jsx
+++ src/components/restaurants.jsx
@@ -5,12 +5,15 @@ export default function Restuarants() {
   const navigate = useNavigate()
   return (<div className="bg-white h-screen text-black text-sm flex  flex-col  items-center overflow-y-auto overflow-x-hidden">
     <section className="">
-      <h1 className="text-5xl text-center p-6 m-4 font-bold text-gray-900">Restuarants Website for You</h1>
+      <h1 className="text-6xl text-center p-6 m-4 font-bold text-gray-900">Restuarants Website for You</h1>
     </section>
+
+    <p className="m-4 text-xl font-semibold text-red-500 text-center">Stop loosing customers to restaurants with websites</p>
+    
     <p className="font-bold text-gray-400 text-2xl text-center">Live in 24hrs. Mobile-friendly Whatsapp order $49.5</p>
 
     <a
-      className="p-4 font-bold text-2xl bg-green-700 text-white px-8 rounded-3xl m-8"
+      className="p-4 font-bold text-xl bg-green-700 text-white px-8 rounded-3xl m-8 shadow-2xl shadow-black"
       href="https://wa.me/231880868634?text=I want a website for my resturant"><FaWhatsapp className="inline" size={40} /> +231 880 868 634</a>
     
     <p className="text-xl text-gray-400 text-center">I Will Reply in 5mins. No Upfront payment</p>
@@ -24,7 +27,7 @@ export default function Restuarants() {
       <h1 className="p-2 text-center text-3xl font-bold m-2 text-green-800">What You get for $49.5</h1>
       <div className="grid grid-cols-2 gap-8">
         <div className="text-center p-6 rounded-2xl shadow-2xl flex flex-col border">
-          <div className="text-8xl text-center m-4">
+          <div className="text-4xl text-center m-4">
             📱
           </div>
           <h2 className="text-center font-bold text-2xl">Mobile First</h2>
@@ -32,7 +35,7 @@ export default function Restuarants() {
         </div>
     
         <div className="text-center p-6 rounded-2xl shadow-2xl flex flex-col border">
-          <div className="text-8xl text-center m-4 text-green-600">
+          <div className="text-4xl text-center m-4 text-green-600">
             💬
           </div>
           <h2 className="text-center font-bold text-2xl">Whatsapp Order</h2>
@@ -40,7 +43,7 @@ export default function Restuarants() {
         </div>
 
         <div className="text-center p-6 rounded-2xl shadow-2xl flex flex-col border">
-          <div className="text-8xl text-center m-4 text-green-600">
+          <div className="text-4xl text-center m-4 text-green-600">
             🚀
           </div>
           <h2 className="text-center font-bold text-2xl">Live in 24hrs</h2>
@@ -48,7 +51,7 @@ export default function Restuarants() {
         </div>
 
         <div className="text-center p-6 rounded-2xl shadow-2xl flex flex-col border">
-          <div className="text-8xl text-center m-4 text-green-600">
+          <div className="text-4xl text-center m-4 text-green-600">
             🌍
           </div>
           <h2 className="text-center font-bold text-2xl">Free Domain Name</h2>
@@ -58,10 +61,9 @@ export default function Restuarants() {
     </section>
 
     <section className="py-16 px-6 text-center">
-      <h1 className="text-2xl font-bold">Ready to go <a className="text-green-600">online</a></h1>
-      <p className="m-4 text-xl font-semibold text-red-500">Stop loosing customers to restaurants with websites</p>
+      <h1 className="text-xl font-bold m-8">Ready to go <a className="text-green-600">online</a></h1>
       <a
-        className="text-white text-3xl p-4 bg-green-600 px-4 shadow-2xl m-4 rounded-xl shadow-black font-mono"
+        className="text-white text-xl p-4 bg-green-600 px-4 shadow-2xl m-4 rounded-xl shadow-black font-mono"
         href="https://wa.me/231880868634?text=I want build a website for my restaurant"><FaWhatsapp className="inline" size={40}/> Whatsap Me Now</a>
     </section>
 
